6aff469340d6384cba0846c81f3390b7c09a6ae9
Annabella Cook <bella@Lovely-Lisa-Cooks-MacBook-Pro.local>
2015-11-21 18:04:50 -0500

    first commit

---
+++ pugs.rb
@@ -0,0 +1,11 @@
+puts new_pug_cuteness
+puts "is the pug cuteness
+puts number_of_pugs
+puts "is the number of pugs"
+puts "each pug has 90 puglets"
+puts "this means that there are "
+puts number_of_puglets
+puts "puglets"
+puts "there are "
+puts number_of_pugs_and_puglets
+puts "pugs altogether"
